317410e259f91f65001ebf577192d727f29e82f6
SteffenLee <tnqjae@naver.com>
2023-07-08 17:09:17 +0900

    Cross entropy error for mini-batch

---
+++ Chapter4/mbCrossEntropyError.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def mini_batch_cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = t.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
