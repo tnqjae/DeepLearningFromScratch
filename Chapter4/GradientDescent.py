18a37e97632a5ae70aa15c4cd192027b1d6a83e0
SteffenLee <tnqjae@naver.com>
2023-07-11 11:59:02 +0900

    Added Gradient Descent code

---
+++ Chapter4/GradientDescent.py
@@ -0,0 +1,13 @@
+import numpy as np
+from GradientEx import numberical_gradient
+
+def gradient_descent(f, init_x, lr = 0.01, step_num = 100):
+    #f는 최적화하려는 함수, init_x는 x의 초깃값, lr -> learing rate로 학습률, step_num은 경사법의 반복 횟수
+    x = init_x #x값 초기화
+
+    for i in range(step_num):
+        grad = numberical_gradient(f, x)# 함수의 기울기를 구하기
+        x -= lr * grad # 학습률을 곱한 값으로 갱신
+        
+    
+    return x
