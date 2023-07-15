Added improved function in numerical_gradient and Fixed incorrect function name change in the previous commit

Chapter4/TwoLayerNet.py
@@ -1,18 +1,8 @@
 import sys, os
 sys.path.append(os.pardir)
-from GradientDescent import numberical_gradient
-from CrossEntropyError import  cross_entropy_error
+from functions import *
+from GradientEx import numberical_gradient
 import numpy as np
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x)) #시그모이드 함수 수식을 통해 구현 exp는 자연상수
-
-def softmax(a): #함수 명명 규칙에 따라 코드를 수정
-    exp_a = np.exp(a) #지수 함수
-    sumExpA = np.sum(exp_a)
-    y = exp_a / sumExpA
-
-    return y
 
 class TwoLayerNet:
     #2층짜리 신경망의 학습 알고리즘 구현
@@ -70,6 +60,32 @@ class TwoLayerNet:
         grads['b1'] = numberical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numberical_gradient(loss_W, self.params['W2'])
         grads['b2'] = numberical_gradient(loss_W, self.params['b2'])
+
+        return grads
+    
+    #numberical_gradient의 개선버전
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+        
+        batch_num = x.shape[0]
+        
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+        
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
 
         return grads
     