3a4015e702feed8f54aab02bc337d3d45971e8b6
SteffenLee <tnqjae@naver.com>
2023-07-16 01:16:21 +0900

    Fix index error and modify code

--- Chapter4/GradientEx.py
+++ Chapter4/GradientEx.py
@@ -1,22 +1,23 @@
 import numpy as np
 
 def numberical_gradient(f, x):
-    h = 1e-4 #0.0001
+    h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
 
-    for idx in range(x.size):
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx]
-
-        #f(x+h)
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        #f(x-h)
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
         grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val #값 복원
-
-    return grad
+        
+        x[idx] = tmp_val # 値を元に戻す
+        it.iternext()   
+        
+        return grad
  
