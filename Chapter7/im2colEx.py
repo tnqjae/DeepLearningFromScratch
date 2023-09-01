f0b3aede74a19c75ffadcaa4529f2c456efe4c71
SteffenLee <tnqjae@naver.com>
2023-09-02 03:33:54 +0900

    Add im2col function example code

---
+++ Chapter7/im2colEx.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+import sys, os
+sys.path.append(os.pardir)
+from util import im2col
+
+x1 = np.random.rand(1, 3, 7, 7)# 데이터 수, 채널 수, 높이, 너비
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape) #(9, 75)
+
+x2 = np.random.rand(10, 3, 7, 7)#데이터 10개
+col2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape)#(90, 75)
