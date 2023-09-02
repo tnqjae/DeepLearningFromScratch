06e5d2f4e65ccdca86cb08fadf9ac65067a6bd5f
SteffenLee <tnqjae@naver.com>
2023-09-03 02:22:41 +0900

    Add Convolution class

---
+++ Chapter7/Conv.py
@@ -0,0 +1,13 @@
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2 * self.pad - FW) / self.stride)
+        out_w = int(1 + (W + 2 * self.pad - Fw) / self.stride)
+
