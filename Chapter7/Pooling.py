eb1c9063e5c594968c637c26d3eee51549db89b0
SteffenLee <tnqjae@naver.com>
2023-09-03 16:58:14 +0900

    Add Pooling class code

---
+++ Chapter7/Pooling.py
@@ -0,0 +1,25 @@
+from util import im2col
+import numpy as np
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad == pad
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(i + (H - self.pool_w) / self.stride)
+
+        #전개 1
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        #최댓값
+        out = np.max(col, axis = 1)
+
+        #성형
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        return out
